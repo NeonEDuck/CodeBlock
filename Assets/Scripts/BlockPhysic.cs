9c521efe5cca81f7e5fcc1afd38bc8956182bce2
NeonEDuck <cj96cj9696cj@gmail.com>
2020-03-29 23:59:47 +0800

    initialize Unity project
    With SampleScene, BlockPhysic script, Sprites and block prefab

---
+++ Assets/Scripts/BlockPhysic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems; 
+
+public class BlockPhysic : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler
+{
+    GameObject parentToReturnTo = null;
+    public void OnBeginDrag( PointerEventData eventData ) {
+        transform.SetParent( transform.parent.parent );
+    }
+
+    public void OnDrag( PointerEventData eventData ) {
+        this.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag( PointerEventData eventData ) {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnDrop( PointerEventData eventData ) {
+        throw new System.NotImplementedException();
+    }
+}
