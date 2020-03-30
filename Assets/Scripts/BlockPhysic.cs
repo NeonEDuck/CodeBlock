862242373e610735ea2c8e2bc5b850f78b2efbb9
PHIL SWIFT <idoissei28@gmail.com>
2020-03-30 17:41:34 +0800

    新增BlockGrid, Block自動新增BlockGrid
    BlockGrid會是Block的父物件
    Block沒有父物件時會新增一個

--- Assets/Scripts/BlockPhysic.cs
+++ Assets/Scripts/BlockPhysic.cs
@@ -1,24 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems; 
+using UnityEngine.EventSystems;
 
 public class BlockPhysic : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler
 {
-    GameObject parentToReturnTo = null;
+    //GameObject parentToReturnTo = null;
+    public GameObject blockGridPrefab = null;
     public void OnBeginDrag( PointerEventData eventData ) {
+        Transform parent = transform.parent;
         transform.SetParent( transform.parent.parent );
+
+        if ( parent.childCount == 0 )
+        {
+            Destroy( parent.gameObject );
+        }
     }
 
     public void OnDrag( PointerEventData eventData ) {
         this.transform.position = eventData.position;
     }
 
-    public void OnEndDrag( PointerEventData eventData ) {
-        throw new System.NotImplementedException();
+    public void OnEndDrag( PointerEventData eventData )
+    {
+        createBlockGrid(eventData.position);
     }
 
     public void OnDrop( PointerEventData eventData ) {
         throw new System.NotImplementedException();
+    }
+
+    void Start()
+    {
+        createBlockGrid( transform.position );
+    }
+
+    void createBlockGrid( Vector3 position )
+    {
+        if (blockGridPrefab == null)
+        {
+            Debug.Log("blockGridPrefab is somehow null!");
+            return;
+        }
+
+        string parent = transform.parent.name;
+
+        if (parent != "BlockGrid")
+        {
+            GameObject blockGrid = Instantiate(blockGridPrefab, transform.parent);
+            blockGrid.transform.position = position;
+            transform.SetParent(blockGrid.transform);
+        }
     }
 }
