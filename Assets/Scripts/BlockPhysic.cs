0ed5137e272704c36d7ac869fa7d926955f5885a
NeonEDuck <cj96cj9696cj@gmail.com>
2020-03-31 21:01:26 +0800

    實作Block插入於Block列功能 將Block邊界大小擴大
    可以將Block拖曳到另一個Block上已進行插入
    如拖曳到Block上半部 將會插入至Block上面
    如拖曳到Block下半部 將會插入至Block下面
    Block邊界上下左右擴大10px (220, 70)

--- Assets/Scripts/BlockPhysic.cs
+++ Assets/Scripts/BlockPhysic.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class BlockPhysic : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler
-{
+public class BlockPhysic : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler {
     //GameObject parentToReturnTo = null;
     public GameObject blockGridPrefab = null;
+    public CanvasGroup canvasGroup = null;
+    public Transform dropOn = null;
+
+    private Vector2 pointerOffset;
+
     public void OnBeginDrag( PointerEventData eventData ) {
         Transform parent = transform.parent;
         transform.SetParent( transform.parent.parent );
@@ -15,19 +20,42 @@ public class BlockPhysic : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
         {
             Destroy( parent.gameObject );
         }
+
+        canvasGroup.blocksRaycasts = false;
+        pointerOffset = new Vector2( transform.position.x, transform.position.y ) - eventData.position;
     }
 
     public void OnDrag( PointerEventData eventData ) {
-        this.transform.position = eventData.position;
+        transform.position = eventData.position + pointerOffset;
     }
 
     public void OnEndDrag( PointerEventData eventData )
     {
-        createBlockGrid(eventData.position);
+
+
+        dropOn = null;
+        canvasGroup.blocksRaycasts = true;
+        createBlockGrid(eventData.position + pointerOffset );
     }
 
     public void OnDrop( PointerEventData eventData ) {
-        throw new System.NotImplementedException();
+
+        //BlockPhysic holding = eventData.pointerDrag.GetComponent<BlockPhysic>();
+        //holding.dropOn = transform;
+
+        if ( eventData.pointerDrag != gameObject ) {
+            GameObject holding = eventData.pointerDrag;
+            Debug.Log( holding.name + " is being drop on " + name );
+            Debug.Log( transform.GetSiblingIndex() );
+
+            holding.transform.SetParent( transform.parent );
+            if ( eventData.position.y < transform.position.y ) {
+                holding.transform.SetSiblingIndex( transform.GetSiblingIndex()+1 );
+            }
+            else {
+                holding.transform.SetSiblingIndex( transform.GetSiblingIndex() );
+            }
+        }
     }
 
     void Start()
@@ -45,7 +73,7 @@ public class BlockPhysic : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
 
         string parent = transform.parent.name;
 
-        if (parent != "BlockGrid")
+        if ( !parent.StartsWith( "BlockGrid" ) )
         {
             GameObject blockGrid = Instantiate(blockGridPrefab, transform.parent);
             blockGrid.transform.position = position;
