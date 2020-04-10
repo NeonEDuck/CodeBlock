b6445fc35d96f39512d3a23b63901729f39bb66c
NEDuck <cj96cj9696cj@gmail.com>
2020-04-10 11:30:15 +0800

    改變堆疊方法
    於其拖曳在方塊上再判斷堆疊於其上或其下
    反而疊在GridBlock上
    可判斷目前是否想放這GridBlock, 並移出空位以致疊放
    GridBlock大小會因目前擁有的方塊而改變
    新增GridBlockDropZone.cs

--- Assets/Scripts/BlockPhysic.cs
+++ Assets/Scripts/BlockPhysic.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class BlockPhysic : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler, IPointerDownHandler {
+public class BlockPhysic : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerDownHandler {
     //GameObject parentToReturnTo = null;
     public GameObject blockGridPrefab = null;
     public CanvasGroup canvasGroup = null;
     public Transform dropOn = null;
+    public Transform placeHolderParent;
 
     private bool draging = false;
 
@@ -18,6 +19,9 @@ public class BlockPhysic : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
     private Transform target;
     private bool holding = false;
     private List<Transform> childList;
+    private GameObject placeHolder;
+    private Transform oldParent;
+    private int maxStack = 0;
 
     public void OnPointerDown( PointerEventData eventData ) {
         onHoldTimer = 0.0f;
@@ -28,110 +32,160 @@ public class BlockPhysic : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
     public void OnBeginDrag( PointerEventData eventData ) {
         target = transform;
         pointerOrigin = eventData.position;
+        maxStack = 0;
+
+        oldParent = transform.parent;
+
+        if (onHoldTimer > 0.25f)
+        {
+
+            placeHolder = new GameObject();
+            placeHolderParent = transform.parent;
+            placeHolder.AddComponent<RectTransform>().sizeDelta = GetComponent<RectTransform>().sizeDelta;
+            placeHolder.transform.SetSiblingIndex(transform.GetSiblingIndex());
+
+            Transform parent = transform.parent;
+            transform.SetParent(transform.parent.parent);
+
+            if (parent.childCount == 0)
+            {
+                Destroy(parent.gameObject);
+            }
+
+            canvasGroup.blocksRaycasts = false;
+            draging = true;
+        }
+        else
+        {
+
+            childList = new List<Transform>();
+
+            Transform newParent = Instantiate(blockGridPrefab, transform.parent).GetComponent<Transform>();
+            newParent.position = transform.position;
+            newParent.SetParent(oldParent.parent);
+
+            for (int i = transform.GetSiblingIndex(); i < oldParent.childCount; i++)
+            {
+                childList.Add(oldParent.GetChild(i));
+            }
+
+            placeHolder = new GameObject();
+            placeHolderParent = transform.parent;
+            placeHolder.AddComponent<RectTransform>().sizeDelta = new Vector2(GetComponent<RectTransform>().sizeDelta.x, GetComponent<RectTransform>().sizeDelta.y * childList.Count);
+            placeHolder.transform.SetSiblingIndex(transform.GetSiblingIndex());
+
+            int j = 0;
+            foreach (Transform child in childList)
+            {
+                child.SetParent(newParent.transform);
+                child.SetSiblingIndex(j++);
+            }
+
+            canvasGroup.blocksRaycasts = false;
+            target = newParent;
+            target.GetComponent<CanvasGroup>().blocksRaycasts = false;
+            draging = true;
+        }
+
+
+        if (oldParent.childCount == 0)
+        {
+            oldParent.gameObject.GetComponent<CanvasGroup>().blocksRaycasts = false;
+        }
     }
 
     public void OnDrag( PointerEventData eventData ) {
 
         // if cursor move, select one block
-        if ( !draging ) {
-            if ( pointerOrigin != eventData.position ) {
-                childList = new List<Transform>();
-                Transform oldParent = transform.parent;
+        target.position = eventData.position + pointerOffset;
 
-                Transform newParent = Instantiate(blockGridPrefab, transform.parent).GetComponent<Transform>();
-                newParent.position = transform.position;
-                newParent.SetParent(oldParent.parent);
+        if (placeHolderParent != null)
+        {
+            placeHolder.transform.SetParent(placeHolderParent);
 
-                for (int i = transform.GetSiblingIndex(); i < oldParent.childCount; i++)
-                {
-                    childList.Add(oldParent.GetChild(i));
-                }
-
-                int j = 0;
-                foreach (Transform child in childList)
-                {
-                    child.SetParent(newParent.transform);
-                    child.SetSiblingIndex(j++);
-                }
-
-                if (oldParent.childCount == 0)
-                {
-                    Destroy(oldParent.gameObject);
-                }
-
-                canvasGroup.blocksRaycasts = false;
-                target = newParent;
-                draging = true;
-            }
-
-            if ( onHoldTimer > 0.25f )
+            for (int i = 0; i < placeHolderParent.childCount; i++)
             {
-                Transform parent = transform.parent;
-                transform.SetParent(transform.parent.parent);
-
-                if (parent.childCount == 0)
+                Transform phpc = placeHolderParent.GetChild(i);
+                if (transform.position.y + phpc.GetComponent<RectTransform>().sizeDelta.y / 2 > phpc.position.y)
                 {
-                    Destroy(parent.gameObject);
+                    placeHolder.transform.SetSiblingIndex(i);
+                    break;
                 }
-
-                canvasGroup.blocksRaycasts = false;
-                draging = true;
             }
         }
-        
-        if ( draging ) {
-            target.position = eventData.position + pointerOffset;
+        else
+        {
+            Debug.Log("null");
         }
     }
 
-    public void OnEndDrag( PointerEventData eventData )
+    public void OnEndDrag(PointerEventData eventData)
     {
         draging = false;
         dropOn = null;
+        target.GetComponent<CanvasGroup>().blocksRaycasts = true;
         canvasGroup.blocksRaycasts = true;
-        createBlockGrid(eventData.position + pointerOffset );
+
+
+        float height = 0f;
+        if (target.gameObject.name.StartsWith("BlockGrid"))
+        {
+
+            if (placeHolderParent != null)
+            {
+                int i = 0;
+                while (target.childCount > 0)
+                {
+                    Transform child = target.GetChild(0);
+                    height += child.GetComponent<RectTransform>().sizeDelta.y;
+                    child.SetParent(placeHolderParent);
+                    child.SetSiblingIndex(placeHolder.transform.GetSiblingIndex() + i++);
+                }
+                Destroy(target.gameObject);
+            }
+            else
+            {
+                for (int i = 0; i < target.childCount; i++)
+                {
+                    height += target.GetChild(i).GetComponent<RectTransform>().sizeDelta.y;
+                }
+                placeHolderParent = transform.parent;
+            }
+        }
+        else
+        {
+
+            if (placeHolderParent != null)
+            {
+                height += GetComponent<RectTransform>().sizeDelta.y;
+                transform.SetParent(placeHolderParent);
+                transform.SetSiblingIndex(placeHolder.transform.GetSiblingIndex());
+            }
+            else
+            {
+                createBlockGrid(eventData.position + pointerOffset);
+                placeHolderParent = transform.parent;
+            }
+        }
+        if (placeHolderParent != null) {
+            Vector2 phpSize = placeHolderParent.GetComponent<RectTransform>().sizeDelta;
+            placeHolderParent.GetComponent<RectTransform>().sizeDelta = new Vector2( Mathf.Max(maxStack * 10f+300f, phpSize.x), phpSize.y + height);
+            placeHolderParent.position = new Vector2(placeHolderParent.position.x, placeHolderParent.position.y - height/2);
+        }
+
+        Vector2 opSize = oldParent.GetComponent<RectTransform>().sizeDelta;
+        oldParent.GetComponent<RectTransform>().sizeDelta = new Vector2(opSize.x, opSize.y - height);
+        oldParent.position = new Vector2( oldParent.position.x , oldParent.position.y + height/2);
+
         holding = false;
         childList = null;
-    }
 
-    public void OnDrop( PointerEventData eventData ) {
+        placeHolder.transform.SetParent(placeHolder.transform.parent.parent);
+        Destroy(placeHolder);
 
-        //BlockPhysic holding = eventData.pointerDrag.GetComponent<BlockPhysic>();
-        //holding.dropOn = transform;
-
-        if ( eventData.pointerDrag != gameObject ) {
-            GameObject holding = eventData.pointerDrag;
-            Debug.Log( holding.name + " is being drop on " + name );
-            Debug.Log( transform.GetSiblingIndex() );
-
-            Transform oldParent = holding.transform.parent;
-
-            int i = 0;
-            foreach ( Transform child in holding.GetComponent<BlockPhysic>().getChildList() ) {
-                Debug.Log( child );
-                child.SetParent( transform.parent );
-                if ( eventData.position.y < transform.position.y ) {
-                    Debug.Log( "Down" );
-                    child.SetSiblingIndex( transform.GetSiblingIndex() + i++ + 1 );
-                }
-                else {
-                    Debug.Log( "Up" );
-                    child.SetSiblingIndex( transform.GetSiblingIndex() );
-                }
-            }
-
-            if ( oldParent.name.StartsWith( "BlockGrid" ) && oldParent.childCount == 0 ) {
-                Destroy( oldParent.gameObject );
-            }
-
-            string codeString = "";
-
-            for (int j = 0; j < transform.parent.childCount; j++)
-            {
-                codeString += transform.parent.GetChild(j).GetComponent<BlockInfo>().getBlockCodeString() + "\n";
-            }
-            Debug.Log(codeString);
-
+        if (oldParent.childCount == 0)
+        {
+            Destroy(oldParent.gameObject);
         }
     }
 
@@ -162,6 +216,7 @@ public class BlockPhysic : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
             GameObject blockGrid = Instantiate(blockGridPrefab, transform.parent);
             blockGrid.transform.position = position;
             transform.SetParent(blockGrid.transform);
+            blockGrid.GetComponent<RectTransform>().sizeDelta = new Vector2(GetComponent<RectTransform>().sizeDelta.x, GetComponent<RectTransform>().sizeDelta.y * ( getChildList().Count + 1) );
         }
     }
 
