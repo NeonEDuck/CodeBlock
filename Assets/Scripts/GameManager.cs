8ae1ea94f386e2f807226609ab6c57378f7db5f1
NEDuck <cj96cj9696cj@gmail.com>
2020-05-19 02:14:38 +0800

    新增指令執行函式, 新增MiniObject, Bug fix
    指令執行函式
    現在可以用define指令增加區域變數
    現在可以用set指令改變區域變數或全域變數的值
    現在可以用move指令指使player物件往哪個方向走
    指令執行間格0.75秒 (還未能自訂)
    指令執行時, 對應的方塊會有白色外框顯示
    
    player物件
    當行走目的地不可跨越, 會待在原地, 否則往前
    
    Bug fix:
    ForBlock, IfBlock的Beam寬度不對

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -22,20 +22,32 @@ public class GameManager : MonoBehaviour {
     public GameObject obstaclePrefab;
     public GameObject playerPrefab;
 
+    [Header( "MiniPrefab" )]
     public Transform gameBoard = null;
     public Transform gameView = null;
-    public short[] gameEnv;
+    public short[] gameEnv; 
+    [HideInInspector] 
+    public Transform[,] gameEnv2d = new Transform[7,6];
+
+    [Header( "Other" )]
+    public Material matHighLight = null;
 
     [Header("Private")]
     public List<Transform> blockGrids = new List<Transform>();
     public List<Transform> blockGridsUnderPointer = new List<Transform>();
+    public Transform player;
     public bool isDraging = false;
     public bool wannaTrash = false;
     public bool showTrashIcon = true;
+    public bool gameStarted = false;
     public int whichStack = 0;
     public Transform preSelectedBlockGrids = null;
+    private List<Dictionary<string, object>> gameVariableLists = new List<Dictionary<string, object>>();
+
 
     public void ResetGameView() {
+        player = null;
+        gameEnv2d = new Transform[7, 6];
         foreach ( Transform child in gameView.transform ) {
             Destroy( child.gameObject );
         }
@@ -50,49 +62,140 @@ public class GameManager : MonoBehaviour {
                         spawn = Instantiate( obstaclePrefab, gameView ).transform;
                         break;
                     case 2:
-                        spawn = Instantiate( playerPrefab, gameView ).transform;
+                        if ( player == null ) {
+                            spawn = Instantiate( playerPrefab, gameView ).transform;
+                            player = spawn;
+                        }
                         break;
                 }
                 if ( spawn != null ) {
-                    spawn.position = origin + new Vector3( ( i % 7 + 0.5f ) * 50f, -( Mathf.Floor( i / 7 ) + 0.5f ) * 50f, 0f );
+                    int x = i % 7;
+                    int y = (int)Mathf.Floor( i / 7 );
+                    gameEnv2d[ x, y ] = spawn;
+                    spawn.GetComponent<MiniGameObject>().posInEnv = new Vector2Int( x, y );
+                    spawn.GetComponent<MiniGameObject>().gameManager = this;
+                    spawn.position = origin + new Vector3( ( x + 0.5f ) * 50f, -( y + 0.5f ) * 50f, 0f );
                 }
             }
         }
     }
 
     public void StartGame() {
-        ResetGameView();
-        List<Tuple<string, List<object>>> commands = CreateCommand();
-        
-        foreach ( Tuple<string, List<object>> command in commands ) {
-            switch ( command.Item1 ) {
-                case "start":
-                    Debug.Log( "start");
-                    break;
-                case "define":
-                    Debug.Log( "define " + command.Item2[0]);
-                    break;
-                case "set":
-                    Debug.Log( "set " + command.Item2[0] + " " + command.Item2[1] );
-                    break;
-                case "for":
-                    Debug.Log( "for " + command.Item2[0] + " " + command.Item2[1] );
-                    break;
-                case "if":
-                    Debug.Log( "if " + command.Item2[0] + " " + command.Item2[1] );
-                    break;
-                case "move":
-                    Debug.Log( "move " + command.Item2[0] + " " + command.Item2[1] );
-                    break;
-            }
-        }
+        if ( !gameStarted ) {
+            gameStarted = true;
+            ResetGameView();
+            List<Tuple<string, Transform, List<object>>> commands = CreateCommand();
+            gameVariableLists.Clear();
+            Debug.Log( "before enter" );
+            StartCoroutine( ExecuteCommand( commands ) );
 
+            //foreach ( Tuple<string, Transform, List<object>> command in commands ) {
+            //    switch ( command.Item1 ) {
+            //        case "start":
+            //            Debug.Log( "start" );
+            //            break;
+            //        case "define":
+            //            Debug.Log( "define " + command.Item3[0] );
+            //            break;
+            //        case "set":
+            //            Debug.Log( "set " + command.Item3[0] + " " + command.Item3[1] );
+            //            break;
+            //        case "for":
+            //            Debug.Log( "for " + command.Item3[0] + " " + command.Item3[1] );
+            //            break;
+            //        case "if":
+            //            Debug.Log( "if " + command.Item3[0] + " " + command.Item3[1] );
+            //            break;
+            //        case "move":
+            //            Debug.Log( "move " + command.Item3[0] + " " + command.Item3[1] );
+            //            break;
+            //    }
+            //}
+
+
+            gameStarted = false;
+        }
     }
 
-    public List<Tuple<string, List<object>>> CreateCommand( Transform target = null ) {
+    public IEnumerator ExecuteCommand( List<Tuple<string, Transform, List<object>>> commands ) {
+        Debug.Log( "enter" );
+        gameVariableLists.Add( new Dictionary<string, object>() );
+        int local = gameVariableLists.Count - 1;
+        string variableName = "";
+        object value = null;
+        double num;
+        WaitForSeconds wait = new WaitForSeconds( 0.75f );
+        foreach ( Tuple<string, Transform, List<object>> command in commands ) {
+            foreach ( Image img in command.Item2.GetComponentsInChildren<Image>() ) {
+                img.material = matHighLight;
+            }
 
-        string output = "";
-        List<Tuple<string, List<object>>> commands = new List<Tuple<string, List<object>>>();
+            switch ( command.Item1 ) {
+                case "start":
+                    // do nothing
+                    break;
+                case "define":
+                    variableName = command.Item3[0].ToString();
+                    if ( !gameVariableLists[local].ContainsKey( variableName ) ) {
+                        gameVariableLists[local].Add( variableName, 0 );
+                    }
+                    else {
+                        gameVariableLists[local][variableName] = 0;
+                    }
+                    break;
+                case "set":
+
+                    variableName = command.Item3[0].ToString();
+                    value = command.Item3[1];
+
+                    for ( int i = local; i >= 0; i-- ) {
+                        if ( !gameVariableLists[i].ContainsKey( variableName ) ) {
+                            continue;
+                        }
+                        else {
+                            if ( value.GetType() == typeof( string ) ) {
+                                if ( double.TryParse( value.ToString(), out num ) ) {
+                                    gameVariableLists[i][variableName] = num;
+                                }
+                                else {
+                                    gameVariableLists[i][variableName] = value;
+                                }
+                                Debug.Log( "variable \"" + variableName + "\" exsit!" );
+                            }
+                            else {
+                                Debug.Log( "Something went wrong!" );
+                            }
+                            break;
+                        }
+                    }
+
+                    break;
+                case "for":
+                    StartCoroutine( ExecuteCommand( (List<Tuple<string, Transform, List<object>>>) command.Item3[0] ) );
+                    break;
+                case "if":
+                    StartCoroutine( ExecuteCommand( (List<Tuple<string, Transform, List<object>>>)command.Item3[0] ) );
+                    StartCoroutine( ExecuteCommand( (List<Tuple<string, Transform, List<object>>>)command.Item3[1] ) );
+                    break;
+                case "move":
+                    if ( double.TryParse( command.Item3[0].ToString(), out num ) ) {
+                        MiniGameObject playerMini = player.GetComponent<MiniGameObject>();
+                        playerMini.Move( (int)num );
+                    }
+                    break;
+            }
+
+            yield return wait;
+
+            foreach ( Image img in command.Item2.GetComponentsInChildren<Image>() ) {
+                img.material = null;
+            }
+        }
+    }
+
+    public List<Tuple<string, Transform, List<object>>> CreateCommand( Transform target = null ) {
+
+        List<Tuple<string, Transform, List<object>>> commands = new List<Tuple<string, Transform, List<object>>>();
 
         if ( target == null ) {
             GameObject[] list = GameObject.FindGameObjectsWithTag( "Start" );
@@ -164,7 +267,7 @@ public class GameManager : MonoBehaviour {
                         break;
                 }
 
-                commands.Add( new Tuple<string, List<object>>( type, infos ) );
+                commands.Add( new Tuple<string, Transform, List<object>>( type, target.GetChild( i ), infos ) );
             }
         }
 
