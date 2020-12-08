b9f0bea7e5ca06e721a1e30e7dbe328ae8fbac90
NeonEDuck <cj96cj9696cj@gmail.com>
2020-12-09 00:02:12 +0800

    更新材質, 建立github連結

--- Assets/Mats/SpriteOutline.shader
+++ Assets/Mats/SpriteOutline.shader
@@ -28,6 +28,13 @@
 		_FrameTex ("Frame Texture", 2D) = "white" {}
 		_ImageOutline ("Outline Color Base", Color) = (1,1,1,1)
 		[KeywordEnum(Stretch, Tile)] _TileMode("Frame mode", Float) = 0
+
+		_StencilComp("Stencil Comparison", Float) = 8
+		_Stencil("Stencil ID", Float) = 0
+		_StencilOp("Stencil Operation", Float) = 0
+		_StencilWriteMask("Stencil Write Mask", Float) = 255
+		_StencilReadMask("Stencil Read Mask", Float) = 255
+		_ColorMask("Color Mask", Float) = 15
     }
 
 	SubShader
--- Web/routes/github.js
+++ Web/routes/github.js
@@ -5,7 +5,30 @@ var pulling = false;
 
 router.get('/', function(req, res, next) {
 	var pass = req.query.pass;
-	console.log(pass);
+	if ( pass === process.env.GIT_PASSWORD ) {
+		if ( pulling === false ) {
+			pulling = true
+			
+			exec("git pull", (error, stdout, stderr) => {
+				if (error) {
+					console.log(`error: ${error.message}`);
+					return;
+				}
+				if (stderr) {
+					console.log(`stderr: ${stderr}`);
+					return;
+				}
+				console.log(`stdout: ${stdout}`);
+			})
+			
+			pulling = false
+		}
+	}
+	res.redirect('back');
+});
+
+router.post('/', function(req, res, next) {
+	var pass = req.query.pass;
 	if ( pass === process.env.GIT_PASSWORD ) {
 		if ( pulling === false ) {
 			pulling = true
