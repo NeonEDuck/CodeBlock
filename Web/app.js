38b0c521a165838946415d89d41e1aff9d932ac0
Haoze <arthur1020099@gmail.com>
2020-12-09 22:10:41 +0800

    測試DNS

--- Web/app.js
+++ Web/app.js
@@ -55,7 +55,7 @@ passport.use(
     new GoogleStrategy({
         clientID: process.env.CLIENT_ID, 
         clientSecret: process.env.CLIENT_SECRET,
-        callbackURL: "http://localhost:80/auth/google/callback"
+        callbackURL: "http://codeblock.ddns.net/auth/google/callback"
     },
     function(accessToken, refreshToken, profile, done) {
         if (profile) {
